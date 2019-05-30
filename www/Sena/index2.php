48fb5ff36bd3e15945a1ae9cd9b9c100933b78ef
jmstevencruz <42804601+jmstevencruz@users.noreply.github.com>
2019-05-30 16:42:34 -0500

    Add files via upload
    Proyecto

---
+++ www/Sena/index2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+	<?php
+	session_start();
+	if (@!$_SESSION['user']) {
+		header("Location:index.php");
+	}elseif ($_SESSION['rol']==1) {
+		header("Location:admin.php");
+	}
+	?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Gestion Usuario</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Joseph Godoy">
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="bootstrap/js/jquery-1.8.3.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+<body data-offset="40" background="images/fondotot.jpg" style="background-attachment: fixed">
+<div class="container">
+<header class="header">
+<div class="row">
+	<?php
+	include("include/cabecera.php");
+	?>
+</div>
+</header>
+
+  <!-- Navbar
+    ================================================== -->
+<?php
+
+include("include/menu.php");
+
+?>
+<!-- ======================================================================================================================== -->
+
+
+		
+	</div>
+	</div>
+	
+	
+
+	</style>
+  </body>
+</html>
